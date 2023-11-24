17a6e277474ec745b28a3c7ff5890376d018f68f
cmottier <camille.mottier@etu.umontpellier.fr>
2023-11-24 17:16:12 +0100

    Horloge : semaine et weekend

--- Par_villes/Horloge.py
+++ Par_villes/Horloge.py
@@ -6,6 +6,7 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from Month import *
 
 #%%
@@ -13,59 +14,60 @@ data = pd.read_csv("Mesure_horaire_(30j)_Region_Occitanie_Polluants_Reglementair
 
 station = 'Montpellier - Prés d Arènes Urbain'
 
-df = table(data, station)
+# Créer le dataframe pour affichage horaire en semaine
 
-df["jour_semaine"] = df.index.day_of_week
-df["heure"] = df.index.hour
-df_semaine = df.loc[(df["jour_semaine"] < 5 ),:]
-df_semaine = df_semaine.drop('jour_semaine', axis = 1)
+def semaine(data, station):
+    df = extraction(data, station)
+    df = df.set_index(["Date"])
+    df["jour_semaine"] = df.index.day_of_week
+    df_semaine = df.loc[(df["jour_semaine"] < 5 ),:]
+    df_semaine = df_semaine.drop('jour_semaine', axis = 1)
+    return df_semaine
 
-df_semaine_polar = (
-    df_semaine.groupby(["heure"])
-    .mean()
-    .reset_index()
-)
+# Créer le dataframe pour affichage horaire en weekend
 
-df_semaine_polar = df_semaine_polar.astype({"heure": str}, copy=False)
+def weekend(data, station):
+    df = extraction(data, station)
+    df = df.set_index(["Date"])
+    df["jour_semaine"] = df.index.day_of_week
+    df_weekend = df.loc[(df["jour_semaine"] >4 ),:]
+    df_weekend = df_weekend.drop('jour_semaine', axis = 1)
+    return df_weekend
 
+# Trace les données horaires en polar
 
-fig = px.line_polar(
-    df_semaine_polar,
-    r="NO2",
-    theta="heure",
-    # color="jour_semaine",
-    # line_close=True,
-    range_r=[0, 50],
-    # start_angle=0,
-    # # color_discrete_sequence=colors,
-    # template="seaborn",
-    # title="Daily accident profile: weekday effect?",
-)
-fig.show()
+def Horloge(df) :
+    df.rename(columns={"nom_poll": "Polluants"}, inplace=True)
+    df["heure"] = df.index.hour
+    df_polar = (
+        df.groupby(["Polluants","heure"])["valeur"]
+        .mean()
+        .reset_index()
+    )
+    df_polar = df_polar.astype({"heure": str}, copy=False)
+    fig = px.line_polar(
+        df_polar,
+        r="valeur",
+        theta="heure",
+        color="Polluants",
+        # line_close=True,
+        range_r=[0, 60],
+    )
+    return fig
 
-# df_polar["jour_semaine"] = df_polar["jour_semaine"].apply(lambda x: calendar.day_abbr[x])
+# Affichage en semaine
 
+def Horloge_semaine(data, station):
+    fig = Horloge(semaine(data, station))
+    fig.update_layout(title="Pollution horaire en semaine")
+    fig.show()
 
-# n_colors = 8  # 7 days, but 8 colors help to have weekends days' color closer
-# colors = px.colors.sample_colorscale(
-#     "mrybm", [n / (n_colors - 1) for n in range(n_colors)]
-# )
+# Affichage en weekend
 
-# for i in df_semaine_polar.columns[1:] :
-#     fig_add = px.line_polar(
-#         df_semaine_polar,
-#         r=i,
-#         theta="heure",
-#     # color="jour_semaine",
-#     # line_close=True,
-#         range_r=[0, 50],
-#     # start_angle=0,
-#     # # color_discrete_sequence=colors,
-#     # template="seaborn",
-#     # title="Daily accident profile: weekday effect?",
-#         fig.show()
-#     )
-
+def Horloge_weekend(data, station):
+    fig = Horloge(weekend(data, station))
+    fig.update_layout(title="Pollution horaire en weekend")
+    fig.show()
 
 
 
