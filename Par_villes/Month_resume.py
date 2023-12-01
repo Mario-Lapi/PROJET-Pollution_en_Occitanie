382964f332983691b30d540891a5928fa7673547
cmottier <camille.mottier@etu.umontpellier.fr>
2023-12-01 14:04:24 +0100

    Valeurs seuils

--- Par_villes/Month_resume.py
+++ Par_villes/Month_resume.py
@@ -26,17 +26,33 @@ def resume(data, station):
     )
     return df_moy
 
+def set_values(row, value):
+    return value[row]
+
+def ajout_norme(df):
+    map_dictionary = {'NO2': 10, 'PM10': 15, 'PM2.5': 5, 'O3': 60, 'NO': 0, 'NOX': 0}
+    df["Seuil"] = df["Polluants"].apply(set_values, args=(map_dictionary,))
+    return df
+
+
 # Trace les données moyennes
 
 def trace_resume(data, station) :
-    df = resume(data, station)
+    df = ajout_norme(resume(data, station))
     df.rename(columns={"valeur": "Concentration moyenne (µg/m³)"}, inplace=True)
     fig = px.line(
         df,
         x = "jour",
-        y = "Concentration moyenne (µg/m³)",
+        y = ["Concentration moyenne (µg/m³)","Seuil"],
         color="Polluants",
     )
+    n = df["Polluants"].nunique()
+    for i in range(n):
+        fig['data'][2*i+1]['line']['dash'] = 'dot'
+    fig.update_layout(
+        xaxis_title="Date", yaxis_title="Concentration moyenne et seuil de référence (µg/m³)"
+    )
+    # fig.update_yaxes(minallowed=0)
     fig.show()
 
 # Pour test :
